577cb9e91af2f3fac60f50e84ce248a1f139ed93
Marcin Mucha <marcin485@op.pl>
2018-11-21 18:02:32 +0100

    Add RoutePlanner class functionality

--- routeplanner.cpp
+++ routeplanner.cpp
@@ -1,6 +1,17 @@
 #include "routeplanner.h"
 
-RoutePlanner::RoutePlanner(QObject *parent) : QObject(parent)
-{
+RoutePlanner::RoutePlanner(QObject *parent) : QObject(parent), lastPosition(), startPoint()
+  ,endPoint()
+{}
 
+RoutePlanner::RoutePlanner(const QGeoCoordinate& firstPosition, const QGeoCoordinate& lastPosition, QObject* parent) : QObject(parent),
+    startPoint(firstPosition), endPoint(lastPosition) {}
+
+void RoutePlanner::setPoint(const QGeoCoordinate& point){
+    if(lastPosition == point)
+        return;
+    else
+        lastPosition = point;
+    emit movePosition();
 }
+
--- routeplanner.h
+++ routeplanner.h
@@ -2,16 +2,29 @@
 #define ROUTEPLANNER_H
 
 #include <QObject>
+#include <QGeoCoordinate>
 
 class RoutePlanner : public QObject
 {
     Q_OBJECT
+    Q_PROPERTY(QGeoCoordinate point READ getStartPos WRITE setPoint NOTIFY movePosition)
+    Q_PROPERTY(QGeoCoordinate startPoint READ getStartPos)
+    Q_PROPERTY(QGeoCoordinate endPoint READ getLastPos)
+
 public:
     explicit RoutePlanner(QObject *parent = nullptr);
+    RoutePlanner(const QGeoCoordinate& firstPosition, const QGeoCoordinate& lastPosition, QObject* parent = nullptr);
+    void setPoint(const QGeoCoordinate& point);
+    inline QGeoCoordinate getPoint(){ return lastPosition; }
+    inline QGeoCoordinate getStartPos(){return startPoint; }
+    inline QGeoCoordinate getLastPos(){return endPoint; }
 
 signals:
-
+    void movePosition();
 public slots:
+private:
+    QGeoCoordinate lastPosition;
+    QGeoCoordinate startPoint, endPoint;
 };
 
 #endif // ROUTEPLANNER_H
