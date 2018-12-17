c5d579f9d53ff0fa7c16c4f4a40c484810d19b85
BWieczorek <blazej.wieczorek9722@gmail.com>
2018-12-17 20:56:14 +0100

    Add MarkerGenerator and Marker as MapItem

---
+++ MarkerGenerator.js
@@ -0,0 +1,29 @@
+var component
+var marker
+
+function createMarkerObjects(coord) {
+    component = Qt.createComponent("MapMarker.qml");
+    if (component.status === Component.Ready)
+        finishCreation(coord);
+    else
+        component.statusChanged.connect(finishCreation);
+
+}
+
+function finishCreation(pos) {
+    if (component.status === Component.Ready) {
+        //marker = component.createObject(map, {"marker.coordinate": map.toCoordinate(Qt.point(posX,posY))});
+        marker = component.createObject(map);
+        marker.coordinate = pos;
+        map.addMapItem(marker);
+        console.log("Object Created " + pos);
+
+        //sprite = component.createObject(appWindow);
+        if (marker === null) {
+            console.log("Error creating Object");
+        }
+    }
+    else if (component.status === Component.Error) {
+        console.log("Error loading component:", component.errorString());
+    }
+}
