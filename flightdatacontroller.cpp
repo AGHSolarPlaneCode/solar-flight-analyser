3fe393db65245887069fee1d08618018d21bd024
Folamomo <noga.igor@gmail.com>
2019-02-18 16:10:01 +0100

    SetUrl backend

--- flightdatacontroller.cpp
+++ flightdatacontroller.cpp
@@ -6,6 +6,7 @@ FlightDataController::FlightDataController(QObject *parent) : QObject(parent)
     QObject::connect(&timer, SIGNAL(timeout()), this, SLOT(StartWorkerIfFree()));
     QObject::connect(&worker, SIGNAL(finished(FlightData)), this, SLOT(workerHasFinished(FlightData)));
     QObject::connect(this, SIGNAL(StartWorker()), &worker, SLOT(start()));
+    QObject::connect(this, SIGNAL(SetWorkerURL(const QUrl&)), &worker, SLOT(setUrl(const QUrl &)));
 
     QObject::connect(this, SIGNAL(StartClock(int)), &timer, SLOT(start(int)));
     QObject::connect(this, SIGNAL(StopClock()),&timer,SLOT(stop()));
@@ -34,6 +35,11 @@ void FlightDataController::doUpdates(bool startflag){
     } else {
         emit StopClock();
     }
+}
+
+void FlightDataController::setUrl(QString url)
+{
+    emit SetWorkerUrl(QUrl(url));
 }
 
 FlightDataAdapter * FlightDataController::getAdapter(){
--- flightdataworker.h
+++ flightdataworker.h
@@ -10,11 +10,11 @@ class FlightDataWorker : public QObject
     Q_OBJECT
 public:
     explicit FlightDataWorker(QObject *parent = nullptr);
-
 signals:
     void finished(FlightData);
 public slots:
     void start();
+    void setUrl(const QUrl& qUrl);
 private:
     ServerManager servermanager{QString("localhost:8080/gps"),this};
     FlightData data;
