a184cf12c764413c66440a1e06cd31d180b924a1
Marcin Mucha <marcin485@op.pl>
2019-06-05 14:19:14 +0200

    Majority of features - (backend)

--- filghtdatagenerator.cpp
+++ filghtdatagenerator.cpp
@@ -3,30 +3,43 @@
 
 
 // default data
-FlightData::Generator::FlightDataGenerator::FlightDataGenerator(const QGeoCoordinate& firstPosition,
-    const QGeoCoordinate& lastPosition, QObject* parent): QObject(parent), flyData{lastPosition,firstPosition,lastPosition,0.0,0.0},
-    weatherData{1,0.0} {}
+FlightData::Generator::FlightDataGenerator::FlightDataGenerator(const QGeoCoordinate& firstPos,
+    const QGeoCoordinate& lastPos, QObject* parent): QObject(parent),
+    flyData{lastPos,firstPos,lastPos,0.0,0.0,0.0},
+    weatherData{1,0.0} {
 
-
+    //qDebug() << flyData.startPoint.distanceTo(flyData.endPoint) /1000;
+}
 
 // FlyData{
-//    QGeoCoordinate lastPosition;
-//    QGeoCoordinate startPoint, endPoint;
-//    double alt;
-//    double speed;
-//};
+//    QGeoCoordinate lastPosition
+//    QGeoCoordinate startPoint, endPoint
+//    double alt
+//    double speed
+//        }
 
 // WeatherData{
-//    int iconID;
-//    double temp;
-//};
+//    int iconID
+//    double temp
+// }
 
 void FlightData::Generator::FlightDataGenerator::setPoint(const QGeoCoordinate& point){
     if(flyData.lastPosition == point)
         return;
     flyData.lastPosition = point;
+
+    setDistanceToPoint(flyData.lastPosition);
+
     generateData();
+
     emit movePosition();  // emit signal about moving position
+}
+
+void FlightData::Generator::FlightDataGenerator::setDistanceToPoint(const QGeoCoordinate& point){
+    if(!point.isValid())
+        return;
+
+    flyData.distanceToGoal = static_cast<double>(flyData.lastPosition.distanceTo(point)) / Data::Param::KM;
 }
 
 void FlightData::Generator::FlightDataGenerator::generateData(){
--- filghtdatagenerator.h
+++ filghtdatagenerator.h
@@ -2,19 +2,26 @@
 #define FILGHTDATAGENERATOR_H
 #include <QGeoCoordinate>
 #include <QObject>
+#include <QtDebug>
 
 namespace FlightData{
+
     namespace Data{
         struct FlyData{
             QGeoCoordinate lastPosition;
             QGeoCoordinate startPoint, endPoint;
             double alt;
             double speed;
+            double distanceToGoal;
         };
 
         struct WeatherData{
             int iconID;
             double temp;
+        };
+
+        struct Param{
+            static constexpr int KM = 1000;
         };
 
     }
@@ -23,24 +30,30 @@ namespace FlightData{
         class FlightDataGenerator : public QObject
         {
             Q_OBJECT
-            Q_PROPERTY(QGeoCoordinate lastPoint READ getPoint WRITE setPoint NOTIFY movePosition)
+            Q_PROPERTY(QGeoCoordinate lastPoint READ getPoint WRITE setPoint NOTIFY movePosition)   // main property which reacts for changes
+
             Q_PROPERTY(QGeoCoordinate startPoint READ getStartPos)
             Q_PROPERTY(QGeoCoordinate endPoint READ getLastPos)
 
             Q_PROPERTY(double lat READ getLat NOTIFY movePosition)
             Q_PROPERTY(double lon READ getLon NOTIFY movePosition)
             Q_PROPERTY(double speed READ getSpeed NOTIFY movePosition)
+            Q_PROPERTY(double distance READ getDistanceToEnd NOTIFY movePosition)
+
         public:
             // default constructor with 'Cracow' -> 'Gdansk' coordinates
             FlightDataGenerator(const QGeoCoordinate& firstPosition = QGeoCoordinate(50.0619474,19.9368564),
             const QGeoCoordinate& lastPosition = QGeoCoordinate(54.347628,18.6452029), QObject* parent = nullptr);
 
-            double getLat()   { return flyData.lastPosition.latitude();  }
-            double getLon()   { return flyData.lastPosition.longitude(); }
-            double getAlt()   { return flyData.alt;   }
-            double getSpeed() { return flyData.speed; }
-            // main setter
+            double getLat() const { return flyData.lastPosition.latitude();  }
+            double getLon() const { return flyData.lastPosition.longitude(); }
+            double getAlt() const { return flyData.alt;   }
+            double getSpeed() const { return flyData.speed; }
+            double getDistanceToEnd() const { return flyData.distanceToGoal; }
+
+            // main setters
             void setPoint(const QGeoCoordinate& point);
+            void setDistanceToPoint(const QGeoCoordinate& point);
             // getters
             inline QGeoCoordinate getPoint()    { return flyData.lastPosition; }
             inline QGeoCoordinate getStartPos() { return flyData.startPoint;   }
