c26fd55d8da2d543b2a2e998322caf4dd8af85e4
Szustarol <szustkarol@gmail.com>
2020-05-05 16:35:33 +0200

    Minor changes in database handler & database table processing (not yet finished)

--- Data/databasetablehandler.cpp
+++ Data/databasetablehandler.cpp
@@ -10,6 +10,19 @@ namespace Data{
         columnNames = database->getTableColumns(this->tableName);
     }
 
+    QString DatabaseTableHandler::getFirstKeyName(int key_idx){
+        QString keyName;
+        for(auto it = columnNames.begin(); it != columnNames.end(); it++){
+            if(it->second != false){
+                if(--key_idx < 0){
+                    keyName = it->first;
+                    break;
+                }
+            }
+        }
+        return keyName;
+    }
+
     DatabaseTableHandler::DatabaseTableHandler(DatabaseConnection * db, const QString & tableName)
     {
         this->database = db;
@@ -22,20 +35,45 @@ namespace Data{
     }
 
     void DatabaseTableHandler::setValueByKey(const QString & key, const QString & column, const QString & value, int key_idx){
-        QString keyName;
-        for(auto it = columnNames.begin(); it != columnNames.end(); it++){
-            if(it->second != false){
-                if(--key_idx < 0){
-                    keyName = it->first;
-                    break;
-                }
-            }
-        }
+        QString keyName = getFirstKeyName(key_idx);
+
+        if(!keyExists(keyName, key))
+            createByKey(keyName, key);
 
         QString keySelector = QString("%1='%2'").arg(keyName).arg(key);
 
 
         database->updateRecord(tableName, keySelector, QString("%1='%2'").arg(column).arg(value));
+    }
+
+
+    void DatabaseTableHandler::setValuesByKey(const QString &key, const std::initializer_list<QString> & columns, const std::initializer_list<QString> & values, int key_idx)
+    {
+        QString keyName =getFirstKeyName(key_idx);
+
+        QString keySelector = QString("%1='%2'").arg(keyName).arg(key);
+
+        QString recordModifier;
+
+        if(!keyExists(keyName, key))
+            createByKey(keyName, key);
+
+        for(auto [itC, itV] = std::tuple{columns.begin(), values.begin()}; itC != columns.end() and itV != values.end(); itC++, itV++){
+            recordModifier += QString("%1='%2',").arg(*itC).arg(*itV);
+        }
+
+        if(recordModifier != "")
+            recordModifier.chop(1);
+
+        database->updateRecord(tableName, keySelector, recordModifier);
+    }
+
+    bool DatabaseTableHandler::keyExists(const QString &keyName, const QString &keyValue){
+        return database->recordExists(tableName, QString("%1='%2'").arg(keyName).arg(keyValue));
+    }
+
+    void DatabaseTableHandler::createByKey(const QString &keyName, const QString &keyValue){
+        database->createRecord(tableName, keyName, keyValue);
     }
 
 }
--- Data/databasetablehandler.h
+++ Data/databasetablehandler.h
@@ -13,9 +13,44 @@ namespace Data {
         QString tableName;
         QVector<QPair<QString, bool>> columnNames;
         void fetchColumns();
+        QString getFirstKeyName(int key_idx);
     public:
+        /*!
+         * \brief keyExists - check if a given primary key exists in a table
+         * \param keyName - name of the key column to check in
+         * \param keyValue - value of the key to look for
+         * \return true if key exists, false otherwise
+         */
+        bool keyExists(const QString & keyName, const QString & keyValue);
+        /*!
+         * \brief createByKey - create an empty record holding just the record value,
+         * this is mostly a helper function
+         * \param keyName - name of the key column to create
+         * \param keyValue - value of the key to use
+         */
+        void createByKey(const QString & keyName, const QString & keyValue);
+        /*!
+         * \brief getNameOfColumn - get name of the column at position columnID
+         * \param columnId
+         * \return name of the column
+         */
         QString getNameOfColumn(unsigned columnId);
+        /*!
+         * \brief setValueByKey - sets a single field in the record (by primary key)
+         * \param key - primary key value
+         * \param column - column to set
+         * \param value - value to set
+         * \param key_idx - optional - index of primary key in the table (if there is more than one). Defaults to 0
+         */
         void setValueByKey(const QString & key, const QString & column, const QString & value, int key_idx = 0);
+        /*!
+         * \brief setValuesByKey - sets multiple fields of a single record
+         * \param key - primary key value
+         * \param columns - initializer list of columns to set
+         * \param values - values to be set in the respective columns
+         * \param key_idx - optional - index of primary key in the table (if there is more than one). Defaults to  0
+         */
+        void setValuesByKey(const QString & key, const std::initializer_list<QString> & columns, const std::initializer_list<QString> & values, int key_idx = 0);
         DatabaseTableHandler(DatabaseConnection * db, const QString & tableName);
     };
 }
